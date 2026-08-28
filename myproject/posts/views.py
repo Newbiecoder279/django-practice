cc283cb1aae42c2f504f2c3c70f7ab8facf7f798
Suvashish Tuladhar <suvashishtuladhar123@gmail.com>
2026-08-28 14:45:28 +0545

    login conditionality features added

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . models import Post
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
@@ -10,4 +10,6 @@ def post_page(request,slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post':post})
 
-def posts_new(request)
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request,'posts/post_new.html')
